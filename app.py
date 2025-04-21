70848c539c6b35ca6820fdad1770501838e72bc1
fmwz <Ahmedwyafooz@gmail.com>
2025-04-21 19:23:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from timezonefinder import TimezoneFinder
 from datetime import datetime
 import pytz
+import os
 
 app = Flask(__name__)
 
@@ -14,23 +15,26 @@ def home():
 
 @app.route('/get_time')
 def get_time():
-    # Get the coordinates from the URL
-    lat = float(request.args.get('lat'))
-    lng = float(request.args.get('lng'))
-    
-    # Find the timezone
-    timezone_str = tf.timezone_at(lng=lng, lat=lat)
-    
-    # Get the time
-    timezone = pytz.timezone(timezone_str)
-    current_time = datetime.now(timezone)
-    
-    # Make the response
-    return jsonify({
-        'time': current_time.strftime('%I:%M %p'),
-        'date': current_time.strftime('%A, %B %d'),
-        'timezone': timezone_str.split('/')[-1].replace('_', ' ')
-    })
+    try:
+        # Get the coordinates from the URL
+        lat = float(request.args.get('lat'))
+        lng = float(request.args.get('lng'))
+        
+        # Find the timezone
+        timezone_str = tf.timezone_at(lng=lng, lat=lat)
+        
+        # Get the time
+        timezone = pytz.timezone(timezone_str)
+        current_time = datetime.now(timezone)
+        
+        # Make the response
+        return jsonify({
+            'time': current_time.strftime('%I:%M %p'),
+            'date': current_time.strftime('%A, %B %d'),
+            'timezone': timezone_str.split('/')[-1].replace('_', ' ')
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run() 
